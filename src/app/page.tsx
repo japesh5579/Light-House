b32470532e7ab31b3bcc0281f2d3ec29a1c45838
japesh5579 <japeshjhatta634@gmail.com>
2026-07-23 23:23:34 +0530

    Revert "Set up shadcn-style component structure and integrate BentoGrid for YTT curriculum"
    This reverts commit 604448e39554f6f63a34c5e783e4286ef75fdd46.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
 import { TildaBody } from "@/components/TildaBody";
-import { YttCurriculumBento } from "@/components/YttCurriculumBento";
-
-const SPLIT_MARKER = "<!--YTT_BENTO_SPLIT-->";
 
 export default function Home() {
   const bodyHtml = fs.readFileSync(
@@ -11,13 +8,5 @@ export default function Home() {
     "utf8"
   );
 
-  const [beforeYtt, afterYtt] = bodyHtml.split(SPLIT_MARKER);
-
-  return (
-    <>
-      <TildaBody html={beforeYtt} />
-      <YttCurriculumBento />
-      <TildaBody html={afterYtt} />
-    </>
-  );
+  return <TildaBody html={bodyHtml} />;
 }
